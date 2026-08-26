ec0d1fabb6681ffec5e800ce29237365376d6487
Pedro Anuda <pedroanuda@gmail.com>
2026-08-25 23:32:39 -0300

    🔧 chore: adicionar prettier, eslint e configurações adiconais

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
     server: {
       port: parseInt(env.PORT) || 5173,
-    }
-  }
-})
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(process.cwd(), 'src'),
+      },
+    },
+  };
+});
