18c7984ee087f57da8d95cf94afb93eb2b041b7c
Pedro Anuda <pedroanuda@gmail.com>
2026-08-29 13:16:06 -0300

    🐛 fix: deploy problems

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
+    base: command === 'build' ? '/ponte-verde/' : '/',
     server: {
       port: parseInt(env.PORT) || 5173,
     },
